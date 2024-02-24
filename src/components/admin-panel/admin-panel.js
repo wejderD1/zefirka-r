26f9fb14d77ccee4106847624018b7fbc5b60612
DartWejder <wejderd1@gmail.com>
2024-02-24 20:08:16 +0100

    24.02

--- src/components/admin-panel/admin-panel.js
+++ src/components/admin-panel/admin-panel.js
@@ -14,59 +14,61 @@ const AdminPanel = ({ newProductCreate }) => {
 
   return (
     <div className="admin-panel">
-      <h1>ADMIN PANEL</h1>
+      <div className="admin-panel__wrapper">
+        <h1>ADMIN PANEL</h1>
 
-      <h2 className="main-text">
-        Create new product item. Insert product title, description and price
-      </h2>
-      <div className="data-input__container">
-        <label className="label" htmlFor="pTitle">
-          title
-        </label>
-        <input
-          className="data-input"
-          type="text"
-          name="pTitle"
-          onChange={onDataChange}
-        />
+        <h2 className="main-text">
+          Create new product item. Insert product title, description and price
+        </h2>
+        <div className="data-input__container">
+          <label className="label" htmlFor="pTitle">
+            title
+          </label>
+          <input
+            className="data-input"
+            type="text"
+            name="pTitle"
+            onChange={onDataChange}
+          />
 
-        <label className="label" htmlFor="pDescription">
-          product description
-        </label>
-        <input
-          className="data-input"
-          type="text"
-          name="pDescription"
-          onChange={onDataChange}
-        />
+          <label className="label" htmlFor="pDescription">
+            product description
+          </label>
+          <input
+            className="data-input"
+            type="text"
+            name="pDescription"
+            onChange={onDataChange}
+          />
 
-        <label className="label" htmlFor="pPrice">
-          product price
-        </label>
-        <input
-          className="data-input"
-          type="text"
-          name="pPrice"
-          onChange={onDataChange}
-        />
+          <label className="label" htmlFor="pPrice">
+            product price
+          </label>
+          <input
+            className="data-input"
+            type="text"
+            name="pPrice"
+            onChange={onDataChange}
+          />
 
-        <label className="label" htmlFor="pImg">
-          product img
-        </label>
-        <input
-          className="data-input"
-          type="text"
-          name="pImg"
-          onChange={onDataChange}
-        />
+          <label className="label" htmlFor="pImg">
+            product img
+          </label>
+          <input
+            className="data-input"
+            type="text"
+            name="pImg"
+            onChange={onDataChange}
+          />
 
-        <button
-          className="btn admin-panel__button"
-          type="button"
-          onClick={() => newProductCreate(productData)}
-        >
-          CREATE
-        </button>
+          <button
+            className="btn admin-panel__button"
+            type="button"
+            onClick={() => newProductCreate(productData)}
+          >
+            CREATE
+          </button>
+        </div>
       </div>
     </div>
   );
