0982497bc05dec3a8fcc2363c3397c0421817549
DartWejder <wejderd1@gmail.com>
2024-04-20 21:22:47 +0200

    add advertising carousel

--- src/components/advertising/advertising.js
+++ src/components/advertising/advertising.js
@@ -5,27 +5,74 @@ import "./advertising.scss";
 function Advertising() {
   return (
     <div className="advertising__container">
-      <buton className="btn btn_arrow btn_arrow-left">&#8249;</buton>
-      <div className="advertising__inner">
-        <div className="advertising__info">
-          <h2 className="main-title advertising__title">
-            Pyszne babeczki wielkanocne
-          </h2>
-          <p className="information__description advertising__description">
-            Czekoladowe jajka, desery z marshmallow, wielkanocne babki i
-            panetone będą doskonałym poczęstunkiem na Wielkanoc.
-            <span>Zamówienia są przyjmowane do 24 marca.</span>
-          </p>
-          <SocialLinks />
+      <ol className="carousell__indicators">
+        <li className="active" data-slide-to="0"></li>
+        <li data-slide-to="1"></li>
+        <li data-slide-to="2"></li>
+      </ol>
+      <div className="carousell__inner">
+        <div className="carousell__slides">
+          <div className="carousell__item">
+            <div className="advertising__info">
+              <h2 className="main-title advertising__title">
+                Pyszne babeczki wielkanocne
+              </h2>
+              <p className="information__description advertising__description">
+                Czekoladowe jajka, desery z marshmallow, wielkanocne babki i
+                panetone będą doskonałym poczęstunkiem na Wielkanoc.
+                <span>Zamówienia są przyjmowane do 24 marca.</span>
+              </p>
+              <SocialLinks />
+            </div>
+            <img
+              // src="http://placehold.it/1600x790"
+              src={require("../../assets/images/advertising_collage.jpg")}
+              alt="картинка рекламы"
+              className="advertising__img img-2"
+            />
+          </div>
+          <div className="carousell__item">
+          <div className="advertising__info">
+              <h2 className="main-title advertising__title">
+                Pyszne babeczki wielkanocne
+              </h2>
+              <p className="information__description advertising__description">
+                Czekoladowe jajka, desery z marshmallow, wielkanocne babki i
+                panetone będą doskonałym poczęstunkiem na Wielkanoc.
+                <span>Zamówienia są przyjmowane do 24 marca.</span>
+              </p>
+              <SocialLinks />
+            </div>
+            <img
+              // src="http://placehold.it/1600x790"
+              src={require("../../assets/images/advertising_collage.jpg")}
+              alt="картинка рекламы"
+              className="advertising__img img-2"
+            />
+          </div>
+          <div className="carousell__item">
+          <div className="advertising__info">
+              <h2 className="main-title advertising__title">
+                Pyszne babeczki wielkanocne
+              </h2>
+              <p className="information__description advertising__description">
+                Czekoladowe jajka, desery z marshmallow, wielkanocne babki i
+                panetone będą doskonałym poczęstunkiem na Wielkanoc.
+                <span>Zamówienia są przyjmowane do 24 marca.</span>
+              </p>
+              <SocialLinks />
+            </div>
+            <img
+              // src="http://placehold.it/1600x790"
+              src={require("../../assets/images/advertising_collage.jpg")}
+              alt="картинка рекламы"
+              className="advertising__img img-2"
+            />
+          </div>
         </div>
-        <img
-          // src="http://placehold.it/1600x790"
-          src={require("../../assets/images/advertising_collage.jpg")}
-          alt="картинка рекламы"
-          className="advertising__img img-2"
-        />
       </div>
-      <buton className="btn btn_arrow btn_arrow-right">&#8250;</buton>
+      <buton className="btn btn_arrow carousell__btn carousell__btn_left">&#8249;</buton>
+      <buton className="btn btn_arrow carousell__btn carousell__btn_right">&#8250;</buton>
     </div>
   );
 }
