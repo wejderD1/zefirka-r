4bb699e6b4504bfaf5b465f1eb271bda1574280c
wejderd1 <wejderd1@wejderd1.home>
2024-08-19 21:06:29 +0200

    add media styles to productsGallery

--- src/components/product-gallery/product-gallery.js
+++ src/components/product-gallery/product-gallery.js
@@ -3,15 +3,17 @@ import "./product-gallery.scss";
 function ProductGallery({ galeryImages }) {
   return (
     <div className="gallery__container">
-      <div className="gallery__inner">
-        {galeryImages.slice(1).map((item, i) => (
-          <img
-            key={i}
-            src={require(`/src/assets/images/products_img/${item}`)}
-            alt="galery-item"
-            className="gallery__item"
-          />
-        ))}
+      <div className="container">
+        <div className="gallery__inner">
+          {galeryImages.slice(1).map((item, i) => (
+            <img
+              key={i}
+              src={require(`/src/assets/images/products_img/${item}`)}
+              alt="galery-item"
+              className="gallery__item"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
